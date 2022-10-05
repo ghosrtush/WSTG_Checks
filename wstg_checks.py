c30e2e62153d30cb2462313aa86531f62481a396
nick <nicholas.giemsa@live.com.au>
2022-10-06 07:36:40 +1000

    added check for robots.txt

--- wstg_checks.py
+++ wstg_checks.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 
 import subprocess
-
-
+import requests
 def whatweb_scan(url):
     whatweb = subprocess.run(['whatweb', url], capture_output=True)
-    return print(whatweb.stdout.decode("utf-8"))
-
-whatweb_scan("google.com")
+    return whatweb.stdout.decode("utf-8")
 
 
-# banner grabbing
+def robots_check(url):
+    robots = requests.get('https://'+url+'/robots.txt')
+    if robots.status_code == 200:
+        return robots.text
+    else:
+        return "Robots.txt not found"
+
+# banner grabbing for  server versions + info leakage
+
+# http headers checks
+
+# cookie status - after login?s
+
+
+# wafw00f - check for WAF in use
+
+
+
+print("[+] WhatWeb Results")
+print(whatweb_scan("google.com"))
+print("[+] Robots.txt check")
+print(robots_check('google.com'))
